bd8daf367792d2a005271a36c5d38942587a1c76
cp985 <166384434+cp985@users.noreply.github.com>
2026-01-31 18:05:10 +0100

    add fetch dummy data cors

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Rotta di test
